1d7a6cadf89bd828b04592f5c0da7c23d51e6f04
Cristiane Maragno <cristianerodriguesmaragno@gmail.com>
2023-05-07 13:51:44 -0300

    Add fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "./../assets/images/logo.png";
 import Dashboard from "./../assets/images/dashboard.png";
+import Image1 from "./../assets/images/image1.png";
+import Image2 from "./../assets/images/image2.png";
+import Image3 from "./../assets/images/image3.png";
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#ffffff] to-[#2BCA24]">
-        <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-20 px-4 py-12 ">
           <div>
             <Image
               src={Logo}
@@ -24,15 +27,17 @@ const Home: NextPage = () => {
               height={56}
             />
           </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 justify-center">
             <div
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/90 p-4 text-grey hover:bg-white/100"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray/100 p-4 text-grey hover:bg-gray/200 justify-center"
             >
-              <div className="text-lg">
-                A cxdata é uma plataforma pensada para unificar o mapeamento da jornada  com inteligência de dados.
+              <div className="text-2xl font-mono">
+                <strong>Reter</strong> seus clientes pode ser <strong>simples</strong> com <strong>inteligência</strong>
               </div>
 
-              <button type="button">Conheça mais!</button>
+              <div className="text-sm font-mono">
+                Solução de inteligência de dados para direcionar o seu sucesso do cliente
+              </div>
             </div>
 
             <div
@@ -48,53 +53,90 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-2 sm:grid-cols-3 md:gap-3">
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:gap-3">
             <div
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/90 p-4 text-grey hover:bg-white/100"
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
             >
-              <div className="text-md">
+              <Image
+                src={Image1}
+                className=""
+                alt="Imagem1"
+                width={500}
+                height={500}
+              />
+            </div>
+
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
+            >
+              <div className="text-md label-feature font-bold text-center">
                 Dados
               </div>
+              <div>
+                Com o CXdata você mapeia os pontos essenciais da jornada do cliente no seu produto e identifica o potencial da sua carteira
+              </div>
+              
             </div>
+          </div>
 
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:gap-3">
             <div
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/90 p-4 text-grey hover:bg-white/100"
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
             >
-              <div className="text-md">
+              <div className="text-md label-feature font-bold text-center">
                 Experiência segmentada
               </div>
+              <div>
+                Amplie sua visão de experiência do cliente. Segmente suas métricas e análises de acordo com o perfil do seu cliente
+              </div>
+              
             </div>
 
             <div
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/90 p-4 text-grey hover:bg-white/100"
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
             >
-              <div className="text-md">
+              <Image
+                src={Image2}
+                className=""
+                alt="Imagem2"
+                width={500}
+                height={500}
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:gap-3">
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
+            >
+              <Image
+                src={Image3}
+                className=""
+                alt="Imagem3"
+                width={500}
+                height={500}
+              />
+            </div>
+
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-grey"
+            >
+              <div className="text-md label-feature font-bold text-center">
                 Prevenção
+              </div>
+              <div>
+                Direcione o seu atendimento à quem precisa de atenção utilizando a inteligência de alertas de cancelamentos
               </div>
             </div>
           </div>
 
-          <div>
-            <div className="text-md">
-              Principais atividades para auxiliá-lo a reter seus clientes
-            </div>
-            <div>
-              - Definir pontos críticos da jornada e alinhar com as métricas e objetivos mais importantes para o seu negócio
-            </div>
-            <div>
-              - Visualizar alertas e receber insights para criar planos de ação direcionados à etapa da jornada do cliente que estão mais longe das metas
-            </div>
-            <div>
-              - Segmentar os perfis dos seus clientes e a parametrização de jornadas e objetivos direcionados
-            </div>
-          </div>
-
-          <div className="items-center justify-center">
-            <div className="text-md">
+          <div className="items-center justify-center text-center">
+            <div className="text-lg text-white font-mono">
               Entre em contato e agende uma conversa
             </div>
             <Link target="_blank" href="https://forms.gle/pMuiGzLRmdSuXeGc6">
-            <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" className="button-contact text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Contato</button>
             </Link>
             
           </div>
